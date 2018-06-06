3fb28cfc59ebac3d32d5388aba2f57bea34eb620
Erik Garces Matos <erik_senac@hotmail.com>
2018-06-06 14:47:57 -0300

    Update PuppeteerSharpPdfDemo.cs

--- PuppeteerSharpPdfDemo.cs
+++ PuppeteerSharpPdfDemo.cs
@@ -1,8 +1,11 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.IO;
 using System.Threading.Tasks;
 using PuppeteerSharp;
+using System.Threading;
+using static System.Net.Mime.MediaTypeNames;
+using System.Net;
 
 namespace PuppeteerSharpPdfDemo
 {
@@ -25,24 +28,47 @@ namespace PuppeteerSharpPdfDemo
             await Downloader.CreateDefault().DownloadRevisionAsync(Downloader.DefaultRevision);
 
             Console.WriteLine("Navigating google");
-            using (var browser = await Puppeteer.LaunchAsync(new LaunchOptions
-            {
-                Headless = false
-            }, Downloader.DefaultRevision))
+            using (var browser = await Puppeteer.LaunchAsync(options, Downloader.DefaultRevision))
             using (var page = await browser.NewPageAsync())
             {
-                await page.SetViewportAsync(new ViewPortOptions{ Width = 1920, Height = 1080});
-                await page.GoToAsync("http://sistemasenem.inep.gov.br/EnemSolicitacao/");
-
-                Console.WriteLine("Generating PDF");
-                //await page.PdfAsync(Path.Combine(Directory.GetCurrentDirectory(), "google.pdf"));
-
-                Console.WriteLine("Export completed");
-
-                if (!args.Any(arg => arg == "auto-exit"))
+                try
                 {
-                    Console.ReadLine();
+
+                    await page.GoToAsync("http://sistemasenem.inep.gov.br/EnemSolicitacao/login.seam");
+                    await page.SetViewportAsync(new ViewPortOptions{ Width = 1920, Height = 920});
+                    //await page.Frames.FirstOrDefault().SelectAsync("#username", "111111");
+                    await page.TypeAsync("#username", "191010104484");
+                    await page.TypeAsync("#password", "UniSL2016");
+                    await page.ClickAsync("input[type='image']");
+
+                    var menu = await page.WaitForSelectorAsync("#LeftMenu #menugroup_4_3 #row_menugroup_4_3");
+                    await menu.ClickAsync();
+                    await page.ClickAsync("#j_id50");
+                    Thread.Sleep(2000);
+                    var inputCPF = await page.WaitForSelectorAsync("#formularioForm");
+                    await page.TypeAsync("#formularioForm input[type='text']", "35168598811");
+                    await page.ClickAsync("#formularioForm input[type='image']");
+                    await page.WaitForSelectorAsync("#resultadoForm input[type='image']");
+                    Thread.Sleep(2000);
+                    await page.ClickAsync("#j_id57");
+
+                    await page.WaitForSelectorAsync("#listaSolicitacaoAtendidas");
+                    Thread.Sleep(2000);
+                    await page.QuerySelectorAsync("#listaSolicitacaoAtendidas tbody tr td:nth-last-child(1) a");
+                    await page.ClickAsync("#listaSolicitacaoAtendidas tbody tr td:nth-last-child(1) a");
+
+                    
+                    //await page.ClickAsync("#resultadoForm input[type='image']");
+                    Console.WriteLine("Terminou!");
+
+                    Console.ReadKey();
                 }
+                catch (Exception ex)
+                {
+                    Console.Write("O erro foi: " + ex);
+                    Console.ReadKey();
+                }
+                
             }
 
         }
